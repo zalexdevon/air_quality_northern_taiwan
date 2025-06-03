d9e9c69bed76220b5c6bfb83497b484498243ea5
Minh Tam <trantam2004hocjava@gmail.com>
2025-06-03 07:50:23 +0700

    update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,21 @@
 import logging
 import os
 from datetime import datetime
+import pytz
 
 
 def configure_logger():
     logs_path = "artifacts/logs"
     os.makedirs(logs_path, exist_ok=True)
     date_format = "%d-%m-%Y-%H-%M-%S"
-    log_file = f"{datetime.now().strftime(date_format)}.log"
+
+    # Get giờ Việt Nam
+    utc_now = datetime.now(pytz.utc)
+    vietnam_tz = pytz.timezone("Asia/Ho_Chi_Minh")
+    vietnam_time = utc_now.astimezone(vietnam_tz)
+
+    # Get tên file
+    log_file = f"{vietnam_time.strftime(date_format)}.log"
     log_file_path = os.path.join(logs_path, log_file)
 
     logging.basicConfig(
